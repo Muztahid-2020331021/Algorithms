74d34ad7a975d7c61a153e43b82d5f82c389c626
Muztahid Hassan <107040850+Muztahid-2020331021@users.noreply.github.com>
2025-02-05 00:33:06 +0600

    Update DSU.cpp

--- Algorithms/DSU.cpp
+++ Algorithms/DSU.cpp
@@ -1,46 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e3+10;
 
-int par[N];
-int size[N];
+class UnionFind{
+    private:
+        vector<int> par, sz;
 
-void make(int v){
-    par[v]=v;
-    size[v]=1;
-}
+    public:
+        UnionFind(int n){
+            par = vector<int> (n);
+            iota(par.begin(), par.end(), 0);
+            sz = vector<int> (n, 1);
+        }
 
-int find(int v){
-    if(par[v]==v) return v;
-    //*path compresion
-    return par[v]=find(par[v]);
-}
+        int find(int v){
+            if(par[v] == v) return v;
+            return par[v] = find(par[v]);
+        }
 
-void euneon(int a, int b){
-    a=find(a);
-    b=find(b);
-    if(a!=b){
-        if(size[a]<size[b])
-            swap(a,b);
-        par[b]=a;
-        size[a]+=size[b];
-    }
-}
+        bool isconnected(int a, int b){
+            a = find(a);
+            b = find(b);
+            return a == b;
+        }
 
-int main(){
-    int n,k;
-    cin>>n>>k;
-    for(int i=1;i<=n;i++){
-        make(i);
-    }
-    while(k--){
-        int u,v;
-        cin>>u>>v;
-        euneon(u,v);
-    }
-
-    int connected_cc=0;
-    for(int i=1;i<=n;i++){
-        if(find(i)==i) connected_cc++;
-    }
-}
+        void join(int a, int b){
+            a = find(a);
+            b = find(b);
+            if(a != b){
+                if(sz[a] < sz[b]){
+                    swap(a, b);
+                }
+                par[b] = a;
+                sz[a] += sz[b];
+            }
+        }
+};
